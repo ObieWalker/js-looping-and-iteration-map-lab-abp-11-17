cccd1b706357dbd25348d608b07d864b2bd2b32f
Obi <obinnawalker@gmail.com>
2017-11-10 00:27:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // Code your solution in this file.
 let drivers = ['Bobby', 'sammy', 'sally', 'annette', 'sarah', 'bobby']
-function lowerCaseDrivers(drivers){
-  drivers.map(function(x){
+function lowerCaseDrivers(drivers).map(function(x){
     return x.toLowerCase()
   });
-}
+
 function nameToAttributes(drivers){
   let driverNames = drivers.split(" ")
-  
+
 
 }
 function attributesToPhrase(drivers).map(function(driver){
