285e19df958537ec679b64ba3330a40b8c084a7e
Obi <obinnawalker@gmail.com>
2017-11-10 00:32:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 let drivers = ['bobby', 'sammy', 'sally', 'annette', 'sarah', 'bobby']
 var lowerCaseDrivers = drivers.map(function(x){
     return x.toLowerCase()
-  });
+  })
 
 function nameToAttributes(drivers){
   let driverNames = drivers.split(" ")
 
 
 }
-function attributesToPhrase(drivers).map(function(driver){
+var attributesToPhrase = drivers.map(function(driver){
   return driver.name+" is from "+driver.hometown
 });
